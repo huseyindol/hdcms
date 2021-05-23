a08f1659f944676dfab700825aa5a6b12b116f1d
huseyindol <huseyindol@gmail.com>
2021-05-23 14:51:11 +0300

    projebilgileri oncelik bilgilendirmeler

---
+++ projebilgileri.js
@@ -0,0 +1,9 @@
+/* TODO:
+ * layout yapisinin tamamlanmasi
+ * mongodb entegresi yapilacak
+ * mongodb schema'si kullanilacak
+ * user schema'si yapildiktan sonra
+ * register sayfasi yapilacak
+ * login sayfasi yapilacak
+ * admin temasi entegresi yapilacak
+ * **/
